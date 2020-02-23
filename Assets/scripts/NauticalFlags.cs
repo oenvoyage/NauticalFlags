3ce3d2c6d54c5e6079b34edb955cd87e1d3b300c
Olivier Amrein <oenvoyage@gmail.com>
2020-02-23 20:52:18 +0100

    Initial commit

---
+++ Assets/scripts/NauticalFlags.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NauticalFlags : MonoBehaviour
+{
+    public Transform letterSpawner;
+    public GameObject letterPrefab;
+
+    public InputField wordList;
+    public string[] words;
+
+    private string word;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        word = "alphabet";
+
+        wordList.text = "beta,racing,test,boat,sailing,marina,ouchy,bateau,maison,lausanne,blender,nautique,blob,cinema,redbull,avion,glenans," +
+            "olivier,lausanne,ouchy,macbook,helice,danger,natation,plage,mer,tilt,golf,house,home,mat,voile,navire,navy,yankee,whisky,zulu,tanguy," +
+            "bravo,minotaure,zelda,zingaro,race,stop,more,less,mouth,hand,main,geneve,montreux,vidy,ordinateur,mecanique,technique,artistique,apprentissage," +
+            "alphabet,bretagne,letters,help,fun,sos,bus,car,eau,camping,orange,punk,funk,vert,bleu,drapeau,livre,aeroport,port,mer,bretagne,nc,phoque," +
+            "chat,chien,camera,ph,ch";
+
+       // wordList.text = "beta,racing,verylongtettesttest";
+
+        newWord();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private string getRandomWord()
+    {
+
+       words = wordList.text.Split(',');
+
+
+        int rand = Random.Range(0, words.Length);
+        string newWord = words[rand];
+
+        Debug.Log("Hidden word  = " + newWord);
+        return newWord;
+    }
+
+    public void newWord()
+    {
+        removeWord();
+        word = getRandomWord();
+        StartCoroutine(generateWord(word));
+    }
+
+    public void showAnswer()
+    {
+        foreach (Transform child in letterSpawner.transform)
+        {
+            // Debug.Log(child.gameObject.name);
+            child.gameObject.GetComponent<oneLetter>().showHint();
+        }
+    }
+
+    public void removeWord()
+    {
+        // foreach (Transform child in letterSpawner.GetComponentsInChildren<Transform>())
+        foreach (Transform child in letterSpawner.transform)
+        {
+            // Debug.Log(child.gameObject.name);
+            child.gameObject.GetComponent<oneLetter>().startDestroying();
+        }
+    }
+
+
+    public IEnumerator generateWord(string word = "nothing")
+    {
+        // Debug.Log("I am now shuffling to this word : " + word);
+        word = word.ToUpper();
+        int index = 0;
+        foreach (var letter in word)
+        {
+            index += 1;
+            // Debug.Log("spawning : " + letter.ToString());
+            GameObject newLetter = Instantiate(letterPrefab,letterSpawner);
+
+            newLetter.GetComponent<oneLetter>().setLetter(letter);
+            newLetter.GetComponent<oneLetter>().index = index;
+            newLetter.name = letter.ToString();
+            newLetter.transform.position = newLetter.transform.position + new Vector3(0.0f, -2.2f * index, 0.0f);
+            yield return new WaitForSeconds(0.2f);
+
+        }
+    }
+}
---
+++ Assets/scripts/oneLetter.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore;
+
+public class oneLetter : MonoBehaviour
+{
+    public int index;
+    public Animator anim;
+    public TextMesh hint;
+
+    public GameObject[] alphabet;
+
+    private bool isHintShown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHintShown = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void startDestroying()
+    {
+
+        Debug.Log("starting kill himself " + gameObject.name);
+
+        StartCoroutine("killHimself"); 
+        
+    }
+
+    private IEnumerator killHimself()
+    {
+        yield return new WaitForSeconds(index * 0.1f);
+        anim.SetTrigger("outro");
+        Destroy(gameObject,0.5f);
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse Touched : " + gameObject.name);
+        toggleHint();
+    }
+
+    public void toggleHint()
+    {
+        if (isHintShown == true)
+        {
+            hideHint();
+        }
+        else
+        {
+           showHint();
+        }
+    }
+    public void hideHint()
+    {
+        isHintShown = false;
+        anim.SetTrigger("hideHint");
+
+    }
+
+
+    public void showHint()
+    {
+        isHintShown = true;
+        anim.SetTrigger("showHint");
+
+    }
+
+
+
+
+
+    public void setLetter(char letter)
+    {
+        hint.text = letter.ToString();
+        // Debug.Log("searching for " + letter.ToString());
+        foreach (GameObject ob in alphabet)
+        {
+            Debug.Log(ob.name);
+            if (ob.name == letter.ToString())
+            {
+                Debug.Log("FOUND");
+                ob.SetActive(true);
+
+            }
+            else
+            {
+                ob.SetActive(false);
+            }
+        }
+    }
+    
+}
